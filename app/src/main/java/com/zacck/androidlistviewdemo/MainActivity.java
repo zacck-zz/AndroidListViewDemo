ea2b3cc37372f501c4344d9b72a1885779a23313
zacck <coderv63@gmail.com>
2016-01-31 07:03:45 +0200

    Added a simple arraylist of family members to show usage of the listview in simple mode

--- app/src/main/java/com/zacck/androidlistviewdemo/MainActivity.java
+++ app/src/main/java/com/zacck/androidlistviewdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.zacck.androidlistviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //lets instantiate the listview
+
+        ListView mDemoListView = (ListView)findViewById(R.id.mDemoListView);
+
+        ArrayList<String> mFamily = new ArrayList<String>();
+        mFamily.add("Isaac");
+        mFamily.add("Samuel");
+        mFamily.add("Janet");
+        mFamily.add("Vanessa");
+        mFamily.add("Rose");
+
+        //Lets Use an array adapter to put the list items in the listview
+        ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mFamily);
+
+        mDemoListView.setAdapter(mArrayAdapter);
+
     }
 }
